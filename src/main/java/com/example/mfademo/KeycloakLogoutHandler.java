51e3ffa1df3fbfd66c28b442a2d463f6c8aba105
Nicky Santiago <nicky.santiago.pagan@gmail.com>
2026-04-07 02:41:38 -0400

    Forgot to add annotation library to Keycloak logout handler file.

--- src/main/java/com/example/mfademo/KeycloakLogoutHandler.java
+++ src/main/java/com/example/mfademo/KeycloakLogoutHandler.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 
 @Component
 public class KeycloakLogoutHandler implements LogoutSuccessHandler {
